c42c8e38635f27ac86955a3ca9156af11fd8dec2
Jack <redjackt@outlook.com>
2020-05-19 15:56:07 -0400

    gitignore

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 # Will Turner 2020
 
-#import Affine
-#import Playfair
-#import Hill
-#import ADFGVX
-#import Vignere
-import Transposition
-#import SubCipher
-from Crypto import ADFGVX
+from Crypto import *
 
 if __name__ == "__main__":
-    print(Transposition.decode("Wm Coreetr al hylrctepiyoo tb!", 5))
-    print(Hill.encode("WHATS UP DUDE", "13 4 1 3"))
-    print(Hill.decode("CRYFWPINZIMD", "13 4 1 3"))
+    t = ColumnarTransposition(cipher="Wm Coreetr al hylrctepiyoo tb!", key=5)
+    print(t.decode())
